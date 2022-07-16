f2c45e64d720e39ae6822b7641bd738fb9cd8f88
Promchai Chooseang <promchai2sin@gmail.com>
2022-07-16 16:27:07 +0700

    Add database module

---
+++ src/lib/database/dbConnect.ts
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const MONGODB_URI = 'mongodb+srv://sin22766:SiN_04027_MongoDB@householdmanager.ptcmgr3.mongodb.net/?retryWrites=true&w=majority'
+
+if (!MONGODB_URI) {
+    throw new Error(
+        'Please define the MONGODB_URI environment variable inside .env'
+    )
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+    cached = global.mongoose = {conn: null, promise: null}
+}
+
+async function dbConnect() {
+    if (cached.conn) {
+        return cached.conn
+    }
+
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: false,
+        }
+
+        cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+            return mongoose
+        })
+    }
+    cached.conn = await cached.promise
+    return cached.conn
+}
+
+export default dbConnect
---
+++ src/lib/database/dbModel.ts
@@ -0,0 +1,36 @@
+import mongoose, {Schema, model} from 'mongoose';
+
+const ProductSchema = new Schema({
+    name: {
+        type: String,
+        unique: true
+    },
+    description: String,
+    amount: Number
+}, {
+    timestamps: true
+});
+
+const OrderSchema = new Schema({
+    lists: {
+        type: [{
+            product: {type: Schema.Types.ObjectId, ref: 'Product'},
+            amount: {
+                type: Number
+            },
+            type: {
+                type: String
+            }
+        }]
+    }
+}, {
+    timestamps: true
+});
+
+const ProductModel = mongoose.models.Product || model('Product', ProductSchema);
+const OrderModel = mongoose.models.Order || model('Order', OrderSchema);
+
+export {
+    ProductModel,
+    OrderModel
+}
